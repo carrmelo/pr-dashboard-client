bf0d30f8c19a55789e35d7895b709d16d1bc12d3
José Manuel <expressmillionaire@hotmail.com>
2018-05-05 19:13:23 +0200

    pull request item

---
+++ src/components/pullrequest/pullrequest_item.js
@@ -0,0 +1,17 @@
+
+import React, { Component } from 'react'
+
+class PullRequestItem extends Component {
+	render() {
+		return (
+			<div>
+				PULL REQUEST ITEM
+			</div>
+		)
+	}
+}
+
+export default PullRequestItem
+
+
+
--- src/components/pullrequest/pullrequest_list.js
+++ src/components/pullrequest/pullrequest_list.js
@@ -1,13 +1,27 @@
 
-
-
 import React, { Component } from 'react'
+import axios from 'axios'
+
+import PullRequestItem from './pullrequest_item'
+
 
 class PullRequestsList extends Component {
+
+	componentDidMount() {
+		console.log('PR LIST')
+	}
+
+	renderPullRequestItem () {
+		console.log('RENDER ITEM')
+	}
+
 	render() {
+		this.renderPullRequestItem()
 		return (
+			
 			<div>
-				PULL REQUEST LIST
+				PULL REQUEST LIST:
+				<PullRequestItem />
 			</div>
 		)
 	}
