469becead0fbe33fbbafd3c7f8e1f601f5ce0d25
José Manuel <expressmillionaire@hotmail.com>
2018-05-05 19:03:42 +0200

    correct PR wiring

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,12 @@
 import React, { Component } from 'react';
-import './App.css';
+
+import PullRequestsList from './pullrequest/pullrequest_list'
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <h1 className="App-title">Pull Requests Dashboard</h1>
-        </header>
+        	<PullRequestsList />	
       </div>
     );
   }
---
+++ src/components/pullrequest/pullrequest_list.js
@@ -0,0 +1,20 @@
+
+
+
+import React, { Component } from 'react'
+
+class PullRequestsList extends Component {
+	render() {
+		return (
+			<div>
+				PULL REQUEST LIST
+			</div>
+		)
+	}
+}
+
+export default PullRequestsList
+
+
+
+
