ce37e78f0624df99d269578843cc8e2db09098dd
José Manuel <expressmillionaire@hotmail.com>
2018-05-08 23:04:04 +0200

    static header, side navigation and repo list card in pannel

--- src/components/layout/header/component_dashboard_header.js
+++ src/components/layout/header/component_dashboard_header.js
@@ -14,17 +14,21 @@ class DashboardHeader extends Component {
 				style={{backgroundColor: 'inherit', boxShadow: 'inherit'}}
 				className="dashboard__header" >
 				
+				<div className="left-items">
 				<svg className="dashboard__logo">
 					<use xlinkHref="./icons/sprite.svg#icon-social-github-circular"></use>
 				</svg>
+				</div>
 				
+				<div className="central-right-items">
 				<form action="#" className="dashboard__header__search">
-					<input type="text" className="dashboard__header__search__input"/>
+					<input type="text" className="dashboard__header__search__input" placeholder="" />
 						<svg className="dashboard__header__search__icon">
 							<use xlinkHref="./icons/sprite.svg#icon-zoom-outline"></use>
 						</svg>
 					<button className="dashboard__header__search__button"></button>
 				</form>
+				</div>
 
 				<button className="dashboard__header__button"></button>
 			</header>
