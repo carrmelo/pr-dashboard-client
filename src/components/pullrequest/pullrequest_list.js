8c6411c1f931e6407dfb9a42739cf602472e84ec
car <carmelogustavo@gmail.com>
2018-05-09 12:08:49 +0200

    refactor: saving some examples of normalizr. no working solution

--- src/components/pullrequest/pullrequest_list.js
+++ src/components/pullrequest/pullrequest_list.js
@@ -3,10 +3,51 @@ import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import axios from 'axios';
 
+import { normalize, schema } from 'normalizr';
+
 import { allPullRequests } from '../../actions';
 
 import config from '../../config';
 import PullRequestItem from './pullrequest_item';
+
+
+const users = new schema.Entity('user', {}, { idAtribute: 'loginName' });
+const repos = new schema.Entity('repository', {}, { idAtribute: 'fullName' });
+const pulls = new schema.Entity('pull_requests', {}, { idAttribute: '_id' } )
+const pullSchema = new schema.Entity('pull_requests', { users: users, repos: repos }, {
+  idAttribute: '_id',
+  mergeStrategy: (entityA, entityB) => ({
+    ...entityA,
+    ...entityB,
+    favorites: entityA.favorites
+  })
+});
+
+const otherSchema = new schema.Entity('pull_requests', {}, { idAttribute: '_id' });
+const pullRequestSchema = [ otherSchema ];
+const theSchema = [ pullSchema ]
+
+const data = { id_str: '123', url: 'https://twitter.com', user: { id_str: '456', name: 'Jimmy' } };
+
+const user = new schema.Entity('users', {}, { idAttribute: 'id_str' });
+const tweet = new schema.Entity('tweets', { user: user }, { 
+    idAttribute: 'id_str',
+    // Apply everything from entityB over entityA, except for "favorites"
+    // mergeStrategy: (entityA, entityB) => ({
+    //   ...entityA,
+    //   ...entityB,
+    //   favorites: entityA.favorites
+    // })
+});
+
+const normalizedData = normalize(data, tweet);
+
+// new schema.Array({
+//   pull_requests: pullSchema,
+//   repositories: repoSchema,
+//   users: userSchema
+// }, (input, parent, key) => `${input._id}`);
+
 
 const socket = io.connect('https://pr-dashboard-server.herokuapp.com/');
 
@@ -15,7 +56,12 @@ class PullRequestList extends Component {
   componentDidMount () {
     socket.on('pr-update', this.props.allPullRequests.bind(this));
     axios.get(`${config.baseServerUrl}/pullrequests`)
-      .then(res => this.props.allPullRequests(res.data))
+      .then(res => {
+        console.log('normalizedData', normalize(res.data, pullSchema));
+        console.log('normalizedData theSchema', normalize(res.data, theSchema));
+        console.log('normalizedData otherSchema', normalize(res.data, pullRequestSchema));
+        console.log('normalizedData Example', normalizedData);
+        this.props.allPullRequests(res.data)})
   };
 
   renderPullRequestItem (pulls) {
