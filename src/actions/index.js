116cc89401954f09da5aca2cbbd435573d86ebc5
car <carmelogustavo@gmail.com>
2018-05-10 10:48:06 +0200

    refactor: implement getPullRequest

--- src/actions/index.js
+++ src/actions/index.js
@@ -5,22 +5,11 @@ export const PULLS_REQUEST = 'PULLS_REQUEST'
 export const PULLS_SUCCESS = 'PULLS_SUCCESS'
 export const PULLS_FAILURE = 'PULLS_FAILURE'
 
-export const TOGGLE_REQUEST = 'TOGGLE_REQUEST'
-export const TOGGLE_SUCCESS = 'TOGGLE_SUCCESS'
-export const TOGGLE_FAILURE = 'TOGGLE_FAILURE'
-
 export const getPullRequests = () => ({
   [CALL_API]: {
     types: [ PULLS_REQUEST, PULLS_SUCCESS, PULLS_FAILURE ],
     endpoint: `${config.baseServerUrl}/pullrequests`,
     schema: Schemas.PULLS
-  }
-})
-
-export const toggleActive = (id, action) => ({
-  [CALL_API]: {
-    types: [TOGGLE_REQUEST, TOGGLE_SUCCESS, TOGGLE_FAILURE ],
-    endpoint: `${config.baseServerUrl}/repos/${id}/${action}`
   }
 })
 
