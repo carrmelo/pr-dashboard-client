80eac766c46cf13a443be968f9bd381ea43b9f4e
g0g11 <goran.plavsic@gmail.com>
2018-05-14 22:41:00 +0200

    feat: deleted config.js

--- src/config.js
+++
@@ -1,7 +0,0 @@
-const config = {
-  baseServerUrl: 'https://pr-dashboard-server.herokuapp.com/v1',
-  baseClientUrl: 'https://pure-client.herokuapp.com',
-  serverUrl: 'https://pr-dashboard-server.herokuapp.com/v1',
-};
-
-export default config;
