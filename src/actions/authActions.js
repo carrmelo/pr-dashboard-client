3fa8c0dc9b404476f52764a7e0979d02dffc0903
g0g11 <goran.plavsic@gmail.com>
2018-05-14 22:51:06 +0200

    feat: config.js removed from authActions

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -1,4 +1,4 @@
-import { CALL_API, Schemas } from '../middleware/api'
+import { CALL_API, Schemas } from '../middleware/api';
 
 import {
   LOGIN_REQUEST,
@@ -6,23 +6,27 @@ import {
   LOGIN_FAILURE,
   USER_INFO_REQUEST,
   USER_INFO_SUCCESS,
-  USER_INFO_FAILURE
+  USER_INFO_FAILURE,
 } from './types';
 
 export const getUserInfo = () => ({
   [CALL_API]: {
-    types: [ USER_INFO_REQUEST, USER_INFO_SUCCESS, USER_INFO_FAILURE ],
-    endpoint: `${REACT_APP_SERVER_URL}/user/me`,
-    schema: Schemas.USER  
-  }
-})
+    types: [USER_INFO_REQUEST, USER_INFO_SUCCESS, USER_INFO_FAILURE],
+    endpoint: `${process.env.REACT_APP_SERVER_URL}${
+      process.env.REACT_APP_API_VERSION
+    }/user/me`,
+    schema: Schemas.USER,
+  },
+});
 
-export const loginUser = (code) => ({
+export const loginUser = code => ({
   [CALL_API]: {
-    types: [ LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE ],
-    endpoint: `${config.serverUrl}/auth/callback${code}`
-  }
-})
+    types: [LOGIN_REQUEST, LOGIN_SUCCESS, LOGIN_FAILURE],
+    endpoint: `${process.env.REACT_APP_SERVER_URL}${
+      process.env.REACT_APP_API_VERSION
+    }/auth/callback${code}`,
+  },
+});
 
 // export const logoutUser = () => ({
 //   type: ActionTypes.LOGOUT
