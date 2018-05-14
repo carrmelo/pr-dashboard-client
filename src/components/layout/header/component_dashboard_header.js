b2fac8ffd5f087fa87f9f8b941958fb8f8919468
g0g11 <goran.plavsic@gmail.com>
2018-05-14 22:40:26 +0200

    feat: implemented dotenv

--- src/components/layout/header/component_dashboard_header.js
+++ src/components/layout/header/component_dashboard_header.js
@@ -1,5 +1,4 @@
-
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 
 import { connect } from 'react-redux';
 import { bindActionCreators } from 'redux';
@@ -7,67 +6,73 @@ import { bindActionCreators } from 'redux';
 import * as authActions from '../../../actions/authActions';
 
 class DashboardHeader extends Component {
-
-	redirect() {
-    window.location.href = `https://pr-dashboard-server.herokuapp.com/v1/auth/github`;
+  redirect() {
+    window.location.href = `{\`${process.env.REACT_APP_SERVER_URL}${
+      process.env.REACT_APP_API_VERSION
+    }/auth/github\`}`;
   }
 
-	handleLogIn = () => {
-		// const msg = loginTab(`https://pr-dashboard-server.herokuapp.com/v1/auth/github`)
-		// msg.then(user => {
-		// 	console.log('USER', user);
-			
-		this.props.authActions.loginUser()
+  handleLogIn = () => {
+    // const msg = loginTab(`https://pr-dashboard-server.herokuapp.com/v1/auth/github`)
+    // msg.then(user => {
+    // 	console.log('USER', user);
 
-	}
+    this.props.authActions.loginUser();
+  };
 
-	handleLogOut = () => {
-		this.props.authActions.logoutUser();
-	}
+  handleLogOut = () => {
+    this.props.authActions.logoutUser();
+  };
 
-	render() {
-		return (
-			<header 
-				style={{backgroundColor: 'inherit', boxShadow: 'inherit'}}
-				className="dashboard__header" >
-				
-				<div className="header__logo">
-					<svg className="header__logo-icon">
-						<use xlinkHref="./icons/sprite.svg#icon-social-github-circular"></use>
-					</svg>
-				</div>
+  render() {
+    return (
+      <header
+        style={{ backgroundColor: 'inherit', boxShadow: 'inherit' }}
+        className="dashboard__header"
+      >
+        <div className="header__logo">
+          <svg className="header__logo-icon">
+            <use xlinkHref="./icons/sprite.svg#icon-social-github-circular" />
+          </svg>
+        </div>
 
-				<div className="header__search">
-					<form action="#" className="header__search-form">
-						<input type="text" className="header__search-input" placeholder="" />
-						<button className="header__search-button">
-							<svg className="header__search-icon">
-								<use xlinkHref="./icons/sprite.svg#icon-zoom-outline"></use>
-							</svg>
-						</button>
-					</form>
-				</div>
+        <div className="header__search">
+          <form action="#" className="header__search-form">
+            <input
+              type="text"
+              className="header__search-input"
+              placeholder=""
+            />
+            <button className="header__search-button">
+              <svg className="header__search-icon">
+                <use xlinkHref="./icons/sprite.svg#icon-zoom-outline" />
+              </svg>
+            </button>
+          </form>
+        </div>
 
-				<a href= 'https://pr-dashboard-server.herokuapp.com/v1/auth/github '>Link </a>
+        <a
+          href={`${process.env.REACT_APP_SERVER_URL}${
+            process.env.REACT_APP_API_VERSION
+          }/auth/github`}
+        >
+          Link{' '}
+        </a>
 
-				<div className="header__config">
-					<button className="header__config-button" onClick={this.handleLogIn}>
-						<svg className="header__config-icon">
-							<use xlinkHref="./icons/sprite.svg#icon-cog-outline"></use>
-						</svg>
-					</button>
-				</div>
-			</header>
-		)
-	}
+        <div className="header__config">
+          <button className="header__config-button" onClick={this.handleLogIn}>
+            <svg className="header__config-icon">
+              <use xlinkHref="./icons/sprite.svg#icon-cog-outline" />
+            </svg>
+          </button>
+        </div>
+      </header>
+    );
+  }
 }
 
 const mapDispatchToProps = dispatch => ({
-	authActions: bindActionCreators(authActions, dispatch)
+  authActions: bindActionCreators(authActions, dispatch),
 });
 
 export default connect(null, mapDispatchToProps)(DashboardHeader);
-
-
-
-
