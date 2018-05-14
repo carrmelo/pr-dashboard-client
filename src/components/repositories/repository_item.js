481d1d8a66034447b20ad75cfad996317900a843
José Manuel <expressmillionaire@hotmail.com>
2018-05-14 16:26:34 +0200

    color picker interactivity

--- src/components/repositories/repository_item.js
+++ src/components/repositories/repository_item.js
@@ -17,72 +17,92 @@ import FlatButton from 'material-ui/FlatButton';
 import RaisedButton from 'material-ui/RaisedButton';
 import Popover from 'material-ui/Popover';
 
-
-import { HuePicker, BlockPicker, GithubPicker, CirclePicker} from 'react-color'
-
-
+import { HuePicker } from 'react-color'
 
 class RepositoryItem extends Component {
-	//active={repo.hookEnabled} // boolean // description // fullName
-	// hookEnabled // name // private // webUrl // _id
+	//active={repo.hookEnabled} // boolean
+	// description
+	// fullName
+	// hookEnabled
+	// name
+	// private
+	// webUrl
+	// _id
 
 	state = {
       open: false,
     };
+    
+	onColorButtonClick = (e) => {
+	    e.preventDefault();
+		const { currentTarget } = e
 
-	onToggleSwitch (repoID) {
-		this.props.repoSwitch(repoID)
-	}
-
-	onColorButtonClick () {
-		console.log('ACTION CREATOR', this.props.selectColor)
-		console.log('REPO ITEM', this.props.repo)
-	}
-
-	  
-
-
-	handleClick = (event) => {
-	    // This prevents ghost click.
-	    event.preventDefault();
-		
-		const { currentTarget } = event
 		setTimeout(() => {
 			this.setState({
 		    	open: true,
 		      	anchorEl: currentTarget,
 		    });
-		}, 500)
+		}, 1000)
+
+		console.log('ACTION CREATOR', this.propsselectColor)
+		console.log('REPO ITEM', this.props.repo)
 	    
   	};
 
-	  handleRequestClose = () => {
-	    this.setState({
-	      open: false,
+	handleRequestClose = () => {
+		this.setState({
+	    	open: false,
 	    });
-	  };
+	};
 
- 	render() {
-    	return (
-	      <div>
-	        <RaisedButton
-	          onClick={this.handleClick}
-	          label="Click me"
-	        />
-	        <Popover
-	          open={this.state.open}
-	          anchorEl={this.state.anchorEl}
-	          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-	          targetOrigin={{horizontal: 'left', vertical: 'top'}}
-	          onRequestClose={this.handleRequestClose}
-	        >
-				<div>
-			    	<HuePicker />
-			    </div>
-	        </Popover>
-	      </div>
-	    );
-  	}
+	onToggleSwitch (repoID) {
+		this.props.repoSwitch(repoID)
+	}
+
+	render() {
+		return (
+			<li className="repository__item">
+				<div className="repository__item-content">
+					<div className="repository__item-content-text">
+						<span>{this.props.repo.fullName}</span>
+					</div>
+					<div className="repository__item-content-toggle">
+						<Toggle 
+							onClick={() => this.onToggleSwitch(this.props.repo._id) }
+						/>
+					</div>
+
+					<div className="repository__item__content__extras">
+						<div className="repository__item__tech">
+							<Chip style={styles.chip}>
+					          <Avatar size={32}></Avatar>
+					          {this.props.repo.language}
+					        </Chip>
+						</div>
+
+					<div className="repository__item__color__picker">
+				        <button
+				          onClick={this.onColorButtonClick}
+				          className="repository__item__color__picker__button"
+				          label="Color"
+				        />
+				        <Popover
+				          open={this.state.open}
+				          anchorEl={this.state.anchorEl}
+				          anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
+				          targetOrigin={{horizontal: 'left', vertical: 'top'}}
+				          
+				          onRequestClose={this.handleRequestClose}
+				        >
+						<HuePicker />
+				        </Popover>
+				    </div>
+
+					</div>
+				</div>
+			</li>
+		)
+	}
 }
 
 const styles = {
@@ -100,125 +120,34 @@ const mapDispatchToProps = (dispatch) => {
 }
 
 export default connect(null, mapDispatchToProps)(RepositoryItem)
-/*
-<div>
-         	
-         </div>
-
-         
-*/
-
-
-
-
-
-    
-  
-
-
-
-// import React from 'react';
-// import RaisedButton from 'material-ui/RaisedButton';
-// import Popover from 'material-ui/Popover';
-// import Menu from 'material-ui/Menu';
-// import MenuItem from 'material-ui/MenuItem';
-
-// export default class PopoverExampleSimple extends React.Component {
-
-//   constructor(props) {
-//     super(props);
-
-//     this.state = {
-//       open: false,
-//     };
-//   }
-
-//   handleClick = (event) => {
-//     // This prevents ghost click.
-//     event.preventDefault();
-
-//     this.setState({
-//       open: true,
-//       anchorEl: event.currentTarget,
-//     });
-//   };
-
-//   handleRequestClose = () => {
-//     this.setState({
-//       open: false,
-//     });
-//   };
 
 //   render() {
+//     const actions = [
+//       <FlatButton
+//         label="Cancel"
+//         primary={true}
+//         onClick={this.handleClose}
+//       />,
+//       <FlatButton
+//         label="Submit"
+//         primary={true}
+//         keyboardFocused={true}
+//         onClick={this.handleClose}
+//       />,
+//     ];
 //     return (
 //       <div>
-//         <RaisedButton
-//           onClick={this.handleClick}
-//           label="Click me"
-//         />
-//         <Popover
+//         <RaisedButton label="Dialog" onClick={this.handleOpen} />
+//         <Dialog
+//           title="Dialog With Actions"
+//           actions={actions}
+//           modal={false}
 //           open={this.state.open}
-//           anchorEl={this.state.anchorEl}
-//           anchorOrigin={{horizontal: 'left', vertical: 'bottom'}}
-//           targetOrigin={{horizontal: 'left', vertical: 'top'}}
-//           onRequestClose={this.handleRequestClose}
+//           onRequestClose={this.handleClose}
 //         >
-//           <Menu>
-//             <MenuItem primaryText="Refresh" />
-//             <MenuItem primaryText="Help &amp; feedback" />
-//             <MenuItem primaryText="Settings" />
-//             <MenuItem primaryText="Sign out" />
-//           </Menu>
-//         </Popover>
+//         </Dialog>
 //       </div>
 //     );
 //   }
 // }
-
-
-// /**
-//  * Dialog with action buttons. The actions are passed in as an array of React objects,
-//  * in this example [FlatButtons](/#/components/flat-button).
-//  *
-//  * You can also close this dialog by clicking outside the dialog, or with the 'Esc' key.
-//  */
-
-
-
-
-
-
-
-
-/*
-
-<li className="repository__item">
-				<div className="repository__item-content">
-					<div className="repository__item-content-text">
-						<span>{this.props.repo.fullName}</span>
-					</div>
-					<div className="repository__item-content-toggle">
-						<Toggle 
-							onClick={() => this.onToggleSwitch(this.props.repo._id) }
-						/>
-					</div>
-
-					<div className="repository__item__content__extras">
-
-						<div className="repository__item__tech">
-							<Chip style={styles.chip}>
-					          <Avatar size={32}></Avatar>
-					          {this.props.repo.language}
-					        </Chip>
-						</div>
-
-						<div className="repository__item__color__picker">
-							<button onClick={() => this.onColorButtonClick() }className="repository__item__color__picker__button"></button>
-						</div>
-
-					</div>
-					
-				</div>
-			</li>
-		*/
 
