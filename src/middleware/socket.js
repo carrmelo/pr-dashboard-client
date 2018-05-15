40704a9d57fbdd90b7444895a44639f740a1209e
Dave Martinez <dkm.coder@gmail.com>
2018-05-15 09:35:50 +0200

    chore: add comments with lingering questions

--- src/middleware/socket.js
+++ src/middleware/socket.js
@@ -1,9 +1,19 @@
 import io from 'socket.io-client';
 
 const socket = url => store => {
+  // TODO: 04 - Should we make sure that the user is authenticated here?
+  // Technically, we are sending a token, this could be checked and ask the
+  // server to save socketID for a specific user. Does that mean that we also
+  // want to destroy in when `socket.on('disconnect', (socket) => {})`?
+  // We might want to get variable in this scope and then update it on each
+  // action dispatch?
+  //
+  // https://auth0.com/blog/auth-with-socket-io/
+  //
   let socket = io(url);
 
   socket.on('message', data => {
+    // TODO: 02 - Should we do HTTP requests here and then dispatch the actions?
     store.dispatch({
       type: data.type + '_received',
       data: data.payload,
@@ -11,6 +21,7 @@ const socket = url => store => {
   });
 
   socket.on('exception', data => {
+    // TODO: 03 - In which case should we use this?
     console.error(data.error);
     store.dispatch({
       type: data.type + '_error',
@@ -19,8 +30,9 @@ const socket = url => store => {
   });
 
   return next => action => {
+    // TODO: 01 - The following console logs are not triggered when dispatching
+    // actions manually from Redux Chrome's extension ?
     console.log('ACTION');
-
     console.log(action);
 
     if(!action.socket) return next(action);
