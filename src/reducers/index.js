3f034230d9d366df7823becc75edb8eb49bd753f
Dave Martinez <dkm.coder@gmail.com>
2018-05-15 16:29:06 +0200

    Feat: Add web socket reducers

--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import authenticationReducer from './authenticationReducer';
 import entitiesReducer from './entitiesReducer';
+import socketsReducer from './socketsReducer';
 
 export default combineReducers({
   entities: entitiesReducer,
-  authentication: authenticationReducer
+  authentication: authenticationReducer,
+  sockets: socketsReducer
 })
--- src/reducers/socketsReducer.js
+++ src/reducers/socketsReducer.js
@@ -10,33 +10,32 @@ export default (state = initialState, action) => {
     // ----------------------------------------------
     // Pull Requests
     // ----------------------------------------------
-    case 'update_pull_requests_sent':
+    case 'pull_request_received':
     return {
       ...state,
-    }
-    case 'update_pull_requests_received':
-    return {
-      ...state,
-    }
-    case 'update_pull_requests_error':
-    return {
-      ...state,
+        repositories: {
+          ...state.repositories,
+          ...action.pulls.entities.repository
+        },
+        pull_requests: {
+          ...state.pull_requests,
+          ...action.pulls.entities.pull_requests
+        },
+        users: {
+          ...state.users,
+          ...action.pulls.entities.user
+        }
     }
 
     // ----------------------------------------------
     // Repositories
     // ----------------------------------------------
-    case 'update_repositories_requests_sent':
+    case 'repositories_received':
     return {
       ...state,
-    }
-    case 'update_repositories_requests_received':
-    return {
-      ...state,
-    }
-    case 'update_repositories_requests_error':
-    return {
-      ...state,
+      repositories: {
+        ...action.response.entities.repositories
+      }
     }
 
   default: return state;
