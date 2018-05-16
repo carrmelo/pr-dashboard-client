f2cf981d1afee7274bec29665ec2e9af80ff9d8f
Dave Martinez <dkm.coder@gmail.com>
2018-05-16 12:31:45 +0200

    Fix: Add logging message

--- src/middleware/socket.js
+++ src/middleware/socket.js
@@ -6,6 +6,7 @@ const socket = url => store => {
   let socket = io(url);
 
   socket.on('message', async data => {
+    console.log(data.payload);
     if (data.type === 'pull_request') {
       const normalizedPulls = await normalize(data.payload, Schemas.PULLS);
       data.payload = normalizedPulls;
