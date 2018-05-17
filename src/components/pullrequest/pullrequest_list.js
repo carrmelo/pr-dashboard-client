53441b791433bcf4738c363ad7e1ef654dc82530
g0g11 <goran.plavsic@gmail.com>
2018-05-17 19:47:43 +0200

    fix: pull requests

--- src/components/pullrequest/pullrequest_list.js
+++ src/components/pullrequest/pullrequest_list.js
@@ -18,38 +18,41 @@ class PullRequestList extends Component {
     }
   }
 
-  renderPullRequestItem (pulls) {
-    return (Object.keys(this.props.pulls).length > 0)
-    ? Object.keys(pulls).map(key => (
-      <div key={pulls[key]._id}>
-        <PullRequestItem
-          repository={pulls[key].repository}
-              webUrl={pulls[key].webUrl}
-              closed_at={pulls[key].closed_at}
-              merged_at={pulls[key].merged_at}
-              created_at={pulls[key].created_at}
-              updated_at={pulls[key].updated_at}
-              user={pulls[key].user}
-              title={pulls[key].title}
-              number={pulls[key].number}
-              state={pulls[key].state}
-              seen={pulls[key].seen}
-              comment={pulls[key].comment}
-              comments={pulls[key].comments}
-        />
-      </div>
-    ))
-    : <p>Seems like there's nothing to show yet</p>
-  } 
-
-  render () {
-    return (
-      <Loader loaded={this.props.loaded}>
-        <div>
-          {this.renderPullRequestItem(this.props.pulls)}
+  renderPullRequestItem(pulls) {
+    return Object.keys(this.props.pulls).length > 0 ? (
+      Object.keys(pulls).map(key => (
+        <div key={pulls[key]._id}>
+          <PullRequestItem
+            repository={pulls[key].repository}
+            webUrl={pulls[key].webUrl}
+            closed_at={pulls[key].closed_at}
+            merged_at={pulls[key].merged_at}
+            created_at={pulls[key].created_at}
+            updated_at={pulls[key].updated_at}
+            user={pulls[key].user}
+            title={pulls[key].title}
+            number={pulls[key].number}
+            state={pulls[key].state}
+            seen={pulls[key].seen}
+            comment={pulls[key].comment}
+            comments={pulls[key].comments}
+          />
         </div>
-      </Loader>
-    )
+      ))
+    ) : (
+      <p>Seems like there's nothing to show yet</p>
+    );
+  }
+
+  render() {
+    return (
+      <div className="dashboard__pullrequest__card">
+        <h4 className="dashboard__pullrequest__title">Pullrequests</h4>
+        <Loader loaded={this.props.loaded}>
+          <ul>{this.renderPullRequestItem(this.props.pulls)}</ul>
+        </Loader>
+      </div>
+    );
   }
 }
 
