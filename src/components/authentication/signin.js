b02bdaf547729a2881d19296985072c73227b70d
car <carmelogustavo@gmail.com>
2018-05-17 09:54:39 +0200

    style: basic landing page

--- src/components/authentication/signin.js
+++ src/components/authentication/signin.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 
+import { GithubLoginButton } from 'react-social-login-buttons';
+
 class SignIn extends Component {
 
   componentDidMount () {
@@ -11,18 +13,35 @@ class SignIn extends Component {
 
   render() {
     return (
-      <div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 651.82 317.54">
-          <title>Asset 1</title>
-          <g id="Layer_2" data-name="Layer 2">
-            <g id="Layer_1-2" data-name="Layer 1">
-              <text class="cls-1" transform="translate(7.14 247.8)">PRD
-              </text>
+      <div className="container">
+        <div className="h-container">
+
+          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 651.82 317.54" width="500">
+            <title>Asset 1</title>
+            <g id="Layer_2" data-name="Layer 2">
+              <g id="Layer_1-2" data-name="Layer 1">
+                <text class="cls-1" transform="translate(7.14 247.8)">PRD
+                </text>
+              </g>
             </g>
-          </g>
-        </svg>
-        
-        <a href={`${process.env.REACT_APP_SERVER_URL}${process.env.REACT_APP_API_VERSION}/auth/github`}>Sign In</a>
+          </svg>
+          <div className="v-container">
+
+            <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 651.82 317.54">
+              <title>Asset 1</title>
+              <g id="Layer_2" data-name="Layer 2">
+                <g id="Layer_1-2" data-name="Layer 1">
+                  <text class="cls-2" transform="translate(7.14 247.8)">follow the repo
+                  </text>
+                </g>
+              </g>
+            </svg>
+
+            <a href={`${process.env.REACT_APP_SERVER_URL}${process.env.REACT_APP_API_VERSION}/auth/github`} className="github-button">
+              <GithubLoginButton />
+            </a>  
+          </div>
+        </div>
       </div>
     )
   }
